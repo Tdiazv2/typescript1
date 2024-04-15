7437344749a20d67a6bc373a25e780def91fd803
Tdiazv2 <t.diazv@uniandes.edu.co>
2024-04-15 11:53:24 -0500

    listo

--- tallertypescript/proyecto/principal.ts
+++ tallertypescript/proyecto/principal.ts
@@ -2,27 +2,27 @@ import { Serie } from './Serie.js';
 import { series } from './datos.js';
 
 let seriesTable: HTMLElement = document.getElementById("series")!;
-let promedioTemporadasElement: HTMLElement = document.getElementById("promedioTemporadas")!;
+let promedio: HTMLElement = document.getElementById("promedioTemporadas")!;
 
-mostrarTablaSeries(series);
-promedioTemporadasElement.innerHTML = `<b>Seasons average:</b> ${promedio(series)}`;
+TablaSeries(series);
+promedio.innerHTML = `<b>Seasons average:</b> ${prom(series)}`;
 
-function mostrarTablaSeries(datosSeries: Serie[]): void {
+function TablaSeries(datosSeries: Serie[]): void {
     let seriesTbody: HTMLElement = document.createElement("tbody");
     for (let serie of datosSeries) {
         console.log("cargo")
-        let trElement: HTMLElement = document.createElement("tr"); 
-        trElement.classList.add("table-light");
-        trElement.innerHTML = `<td>${serie.id}</td>
+        let tr: HTMLElement = document.createElement("tr"); 
+        tr.classList.add("table-light");
+        tr.innerHTML = `<td>${serie.id}</td>
         <td>${serie.nom} </td>
         <td>${serie.canal}</td>
         <td>${serie.temporadas}</td>`;
-        seriesTbody.appendChild(trElement);
+        seriesTbody.appendChild(tr);
     }
     seriesTable.appendChild(seriesTbody);
 }
 
-function promedio(series: Serie[]): number {
+function prom(series: Serie[]): number {
     let sum: number = 0;
     series.forEach(serie => {
         sum += serie.temporadas;
